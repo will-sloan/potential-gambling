9a174e74bc3b2a557732148360566ee4452519ec
Will Sloan <willis5552@gmail.com>
2020-06-20 11:06:15 -0400

    Currently has no errors but probably doesn't work

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod utils;
 
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -145,7 +146,14 @@ impl Game {
         }
     }
 
-    fn check_cards(players: &Vec<Player>, flop: &Option<Vec<Card>>) -> Player {
+    fn check_cards(&self, players: &Vec<Player>, flop: &Vec<Card>) -> Player {
+        let mut points: HashMap<String, u8> = HashMap::new();
+        for player in players {
+            let flush = check_flush(&player.cards, flop);
+            let straight = check_straight(&player.cards, flop);
+            let pairs = check_pairs(&player.cards, flop);
+            if flush == 17 && straight == 18 {}
+        }
         Player {
             cards: [Card {
                 suit: Suit::Diamonds,
@@ -158,105 +166,132 @@ impl Game {
             hand: 0,
         }
     }
-
-    fn number_of_pairs(hand: &Vec<Card>, flop: &Vec<Card>) -> u8 {
-        let mut card_nums: Vec<u8> = Vec::new();
-        for card in flop {
-            card_nums.push(card.number);
-        }
-        let mut counter = 0;
-        let mut oneCard: bool = false;
-        // Check if same, so later checks are easier
-        let card_one;
-        let card_two;
-        if hand.first().unwrap().number == hand.last().unwrap().number {
-            oneCard = true;
-        }
-        // If there is only one unique card
-        if oneCard {
-            counter += flop
-                .iter()
-                .filter(|x| x.number == hand.first().unwrap().number)
-                .collect::<Vec<&Card>>()
-                .len()
-                + 1;
-            match counter {
-                _ => 0,
-                1 => 15,
-                2 => 19,
-            }
-        } else {
-            card_one = flop
-                .iter()
-                .filter(|x| x.number == hand.first().unwrap().number)
-                .collect::<Vec<&Card>>()
-                .len();
-            card_two = flop
-                .iter()
-                .filter(|x| x.number == hand.last().unwrap().number)
-                .collect::<Vec<&Card>>()
-                .len();
-            if card_one == 1 || card_two == 1 {
-                return 13;
-            } else if card_one == 1 && card_two == 1 {
-                return 14;
-            } else if card_one == 2 || card_two == 2 {
-                return 15;
-            } else if card_one == 3 || card_two == 3 {
-                return 19;
-            } else {
-                return 0;
-            }
-        }
+}
+fn check_pairs(hand: &Vec<Card>, flop: &Vec<Card>) -> u8 {
+    let mut card_nums: Vec<u8> = Vec::new();
+    for card in flop {
+        card_nums.push(card.number);
     }
-
-    fn check_straight(hand: &Vec<Card>, flop: &Vec<Card>) -> u8 {
-        let mut temp_group: Vec<u8> =
-            [hand.last().unwrap().number, hand.first().unwrap().number].to_vec();
-        let a = temp_group.contains(&12); // check 1,2,3,4,5 and 10,11,12,13,14
-        for i in flop {
-            temp_group.push(i.number);
+    let mut counter = 0;
+    let mut oneCard: bool = false;
+    // Check if same, so later checks are easier
+    let card_one;
+    let card_two;
+    if hand.first().unwrap().number == hand.last().unwrap().number {
+        oneCard = true;
+    }
+    // If there is only one unique card
+    if oneCard {
+        counter += flop
+            .iter()
+            .filter(|x| x.number == hand.first().unwrap().number)
+            .collect::<Vec<&Card>>()
+            .len()
+            + 1;
+        match counter {
+            _ => 0,
+            1 => 15,
+            2 => 19,
         }
-        if temp_group.len() >= 5 {
-            temp_group.sort();
-            if a {
-                // then check 1,2,3,4,5 explicitly
-                for i in 0..5 {
-                    if i as u8 != temp_group[i] {
-                        return 0;
-                    }
-                }
-                return 16;
-            }
-            for i in 0..temp_group.len() - 2 {
-                if (i + 1) as u8 != temp_group[i + 1] {
-                    return 0;
-                }
-            }
-            return 16;
+    } else {
+        card_one = flop
+            .iter()
+            .filter(|x| x.number == hand.first().unwrap().number)
+            .collect::<Vec<&Card>>()
+            .len();
+        card_two = flop
+            .iter()
+            .filter(|x| x.number == hand.last().unwrap().number)
+            .collect::<Vec<&Card>>()
+            .len();
+        if card_one == 1 || card_two == 1 {
+            return 13;
+        } else if card_one == 1 && card_two == 1 {
+            return 14;
+        } else if card_one == 1 && card_two == 2 {
+            return 18;
+        } else if card_one == 2 && card_two == 1 {
+            return 18;
+        } else if card_one == 2 || card_two == 2 {
+            return 15;
+        } else if card_one == 3 || card_two == 3 {
+            return 19;
         } else {
             return 0;
         }
     }
+}
 
-    fn check_flush(hand: &Vec<Card>, flop: &Vec<Card>) -> u8 {
-        let mut suit_to_check_one: Suit = Suit::Error;
-        let mut suit_to_check_two: Suit = Suit::Error;
-        let mut cards: Vec<Suit> = [hand.last().unwrap().suit, hand.last().unwrap().suit].to_vec();
-        for i in flop {
-            cards.push(i.suit);
+fn check_straight(hand: &Vec<Card>, flop: &Vec<Card>) -> u8 {
+    let mut temp_group: Vec<u8> =
+        [hand.last().unwrap().number, hand.first().unwrap().number].to_vec();
+    let a = temp_group.contains(&12); // check 1,2,3,4,5 and 10,11,12,13,14
+    for i in flop {
+        temp_group.push(i.number);
+    }
+    if temp_group.len() >= 5 {
+        temp_group.sort();
+        if a {
+            // then check 1,2,3,4,5 explicitly
+            for i in 0..5 {
+                if i as u8 != temp_group[i] {
+                    return 0;
+                }
+            }
+            return 16;
         }
-        if hand.last().unwrap().suit == hand.last().unwrap().suit {
-            //suit_to_check_one = hand.last().unwrap().suit;
-            return cards
-                .iter()
-                .filter(|x| **x == hand.last().unwrap().suit)
-                .collect::<Vec<&Suit>>()
-                .len() as u8;
+        for i in 0..temp_group.len() - 2 {
+            if (i + 1) as u8 != temp_group[i + 1] {
+                return 0;
+            }
+        }
+        return 16;
+    } else {
+        return 0;
+    }
+}
+
+fn check_flush(hand: &Vec<Card>, flop: &Vec<Card>) -> u8 {
+    let mut occurances_card_one: u8 = 0;
+    let mut occurances_card_two: u8 = 0;
+    let straight = check_straight(hand, flop) == 16;
+    let mut cards: Vec<Suit> = [hand.last().unwrap().suit, hand.last().unwrap().suit].to_vec();
+    for i in flop {
+        cards.push(i.suit);
+    }
+
+    if hand.last().unwrap().suit == hand.last().unwrap().suit {
+        //suit_to_check_one = hand.last().unwrap().suit;
+        if cards
+            .iter()
+            .filter(|x| **x == hand.last().unwrap().suit)
+            .collect::<Vec<&Suit>>()
+            .len() as u8
+            >= 5
+        {
+            if straight {
+                return 20;
+            } else {
+                return 17;
+            }
         } else {
-            suit_to_check_one = hand.last().unwrap().suit;
-            suit_to_check_two = hand.first().unwrap().suit;
-            0
+            return 0;
+        }
+    } else {
+        occurances_card_one = cards
+            .iter()
+            .filter(|x| **x == hand.last().unwrap().suit)
+            .collect::<Vec<&Suit>>()
+            .len() as u8;
+        occurances_card_two = cards
+            .iter()
+            .filter(|x| **x == hand.first().unwrap().suit)
+            .collect::<Vec<&Suit>>()
+            .len() as u8;
+        if occurances_card_one >= 5 || occurances_card_two >= 5 {
+            return 16;
+        } else {
+            return 0;
         }
     }
 }
